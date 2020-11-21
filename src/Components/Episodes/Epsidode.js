511d1df684a860c34988e2ae55b8e4b7f813c7db
DCartist <pbannerman13@gmail.com>
2020-11-21 13:47:58 -0500

    FLAME-25 added Instrctions to Episode Component

--- src/Components/Episodes/Epsidode.js
+++ src/Components/Episodes/Epsidode.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Ingredients from './Ingredients'
 import Tools from "./Tools"
+import Instruction from "./Instructions"
 function Episode(props){
     return (
         <div>
             <h1>{props.title}</h1>
             {props.episode_description}
     {props.drinks.map((item, index)=>(<div><h4>{item.drink_name} </h4>
-        <Ingredients {...item.ingredients} key={index}></Ingredients>
-        <Tools {...item.tools} key={index}></Tools>
+        <Ingredients {...item.ingredients}></Ingredients>
+        <Tools {...item.tools}></Tools>
+        <Instruction instructions = {item.instructions}></Instruction>
     </div>))}
         </div>
     )
