4a8c6a9b7568812613b752f24c56ea2014a43020
DCartist <pbannerman13@gmail.com>
2020-11-21 17:12:48 -0500

    FLAME-9 FLAME-8 Video display added

--- src/Components/Episodes/Video.js
+++ src/Components/Episodes/Video.js
@@ -1,14 +1,16 @@
 import react from 'react';
 import { Embed } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css'
 
 function VideoDisplay(props){
+let placeholderName = process.env.PUBLIC_URL + '/images/clicktoview._animation.gif'
 if (props.videourl.length == 0){
     return(<img src={process.env.PUBLIC_URL + '/images/nophotoavailable_cropped.gif'}/>)
 } else {
     return(
         <Embed
         id={props.video_id}
-        placeholder='/images/vimeo-example.jpg'
+        placeholder={placeholderName}
         source='vimeo'
       />
     )
