b90daf1693bbf0273363a2101f806cb49a2c0140
DCartist <pbannerman13@gmail.com>
2020-11-21 13:27:10 -0500

    FLAME-26 Adding Home Section

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import {Route} from "react-router-dom";
 import EpsidodeListing from "./Components/Episodes/EpisodeListing"
 import Episode from "./Components/Episodes/EpisodeMain"
+import Home from "./Components/Home/Home"
 function App() {
   return (
     <div className="App">
       
+      <Route path="/" exact component={Home}></Route>
       <Route path="/episode" exact component={EpsidodeListing}></Route>
       <Route path="/episode/id/:id" component={Episode}></Route>
     </div>
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                    <img src={process.env.PUBLIC_URL + '/images/flammablehorizon_cropped.gif'} /> 
+            </div>
+        );
+    }
+}
+
+export default Home;
