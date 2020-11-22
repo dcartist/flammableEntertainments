abdcec0db10fc74101a2d141cd58575de90cf17a
DCartist <pbannerman13@gmail.com>
2020-11-22 18:12:39 -0500

    FLAME-1 FLAME-17 adding image to main page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+   
     render() {
+        const divBackground={
+            backgroundImage: `url(${process.env.PUBLIC_URL}/images/drinks.jpg)`
+        }
         return (
-            <div>
-                    {/* <img src={process.env.PUBLIC_URL + '/images/flammablehorizon_cropped.gif'} />  */}
+            <div className="homeBase" style={divBackground}>
+                
+                <div className="homeBackground"></div>
+                HOME
+                    {/* <img src={process.env.PUBL
+                        IC_URL + '/images/flammablehorizon_cropped.gif'} />  */}
             </div>
         );
     }
