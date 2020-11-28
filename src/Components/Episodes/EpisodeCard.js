b925466c2274d95893a8154084e2e23dad05d630
DCartist <pbannerman13@gmail.com>
2020-11-27 21:10:07 -0500

    FLAME-33 Adjusted Images

--- src/Components/Episodes/EpisodeCard.js
+++ src/Components/Episodes/EpisodeCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import { Card, Segment, Header, Image } from 'semantic-ui-react'
-
+import ProductCard from "react-ui-cards" 
 function EpisodeCard(props){
     return (
         <div key={props.key}>
              <Link to={"/episode/id/"+props.id}>
+                 <div>
+                 
             <Card raised>
-            <Image src={props.acf.episode_photo} wrapped ui={true} size='medium'  as='div' alt={props.acf.title} className="EpisodeListImage"/>
+            <div style={{backgroundImage: `url(${props.acf.episode_photo})`}} className="EpisodeListImage"> </div>
+            {/* <Image src={props.acf.episode_photo} wrapped ui={false} alt={props.acf.title} as='div' className="EpisodeListImage"/> */}
             <Card.Content>
             <Segment inverted fluid color="black"><Header as="h4">{props.acf.title}</Header></Segment>
         {/* <Card.Header color="black" inverted>{props.acf.title}</Card.Header> */}
@@ -17,6 +20,7 @@ function EpisodeCard(props){
         </Card.Description>
       </Card.Content>
             </Card>
+            </div>
            </Link>
         </div>
     )
