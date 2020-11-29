33623e32361d053cfb8a556608a7baebf2280046
DCartist <pbannerman13@gmail.com>
2020-11-29 13:32:30 -0500

    Working on Menu

--- src/App.js
+++ src/App.js
@@ -5,21 +5,39 @@ import Episode from "./Components/Episodes/EpisodeMain"
 import Home from "./Components/Home/Home"
 import About from "./Components/About/About"
 import Navigation from "./Components/Navigation/Navigation"
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor(props){
+    super();
+    this.state={
+      menuOpen:false,
+    }
+  }
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation className="AppNav"></Navigation>
-      <Route path="/" exact component={Home}></Route>
-      <Route path="/about" component={About}></Route>
-      <Route path="/episode" exact component={EpsidodeListing}></Route>
-      <Route path="/episodes" exact component={EpsidodeListing}></Route>
-      <Route path="/episode/id/:id" component={Episode}></Route>
-    </div>
-
+  handleMenuClick() {
+    this.setState({menuOpen:!this.state.menuOpen});
+  }
   
-  );
-}
+  handleLinkClick() {
+    this.setState({menuOpen: false});
+  }
 
-export default App;
+  render() {
+
+
+    return (
+      <div className="App">
+        <Navigation className="AppNav"></Navigation>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/episode" exact component={EpsidodeListing}></Route>
+        <Route path="/episodes" exact component={EpsidodeListing}></Route>
+        <Route path="/episode/id/:id" component={Episode}></Route>
+      </div>
+  
+    
+    );
+  }
+}
