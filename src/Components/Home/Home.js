bcd92d74bb53445b57073c46cf2954e1dc2cb113
dcartist <pbannerman13@gmail.com>
2021-11-26 23:00:07 -0500

    adding scss

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,10 +7,16 @@ class Home extends Component {
             backgroundImage: `url(${process.env.PUBLIC_URL}/images/drinks.jpg)`
         }
         return (
-            <div className="homeBase" style={divBackground}>
-                
+            <div>
+                <div className="homeBase" style={divBackground}>
                 <div className="homeBackground"></div>
-             
+                </div>
+                <section id='things' className="tester">
+      <h2>Things</h2>
+    </section>
+    <section id='stuff' className="tester">
+      <h2>Stuff</h2>
+    </section>
                     {/* <img src={process.env.PUBL
                         IC_URL + '/images/flammablehorizon_cropped.gif'} />  */}
             </div>
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
 // import { Menu } from 'semantic-ui-react'
 import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
-
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 class Navigation extends Component {
     state = {}
 
@@ -11,7 +11,15 @@ class Navigation extends Component {
         const { activeItem } = this.state
     return (
         <div className="NavBackground">
-            <div> <img src={process.env.PUBLIC_URL + '/images/flammablehorizon_cropped.gif'} /></div>
+            <div> <img src={process.env.PUBLIC_URL + '/images/flammablehorizon_cropped.gif'} /></div> 
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/">Home</Link></li>
+              <AnchorLink href='#things'>Things</AnchorLink>
+    <AnchorLink href='#stuff'>Stuff</AnchorLink>
+              </ul>
+            
             <div> <Menu stackable size='medium'>
         <Menu.Item
           name='home'
